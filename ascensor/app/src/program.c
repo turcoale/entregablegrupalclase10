1851faad7555ffc1ab69ad6ddc94996da7ddb069
turcoale <amidale@gmail.com>
2017-04-13 21:53:14 -0300

    Probando comentarios

--- ascensor/app/src/program.c
+++ ascensor/app/src/program.c
@@ -1,35 +1,9 @@
-/* Copyright (c) 2017, Eric Pernia.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- * ============================================================================
- * Date: 2017-03-15
- * ============================================================================
+/*Ejercicio Entregable 10
+Autores:
+        Gessi Paniagua
+        Guillermo López
+        Amid Ale
+Implementación de MEF en un ascensor
  */
 
 /*==================[inclusions]=============================================*/
@@ -54,23 +28,23 @@ int main( void ){
 
    // ---------- SETUP ----------------------------------------
    // Initialize an configurate the board
-   boardConfig();   
-   
+   boardConfig();
+
    // ---------- REPEAT FOR EVER ------------------------------
    while( TRUE )
    {
       // If TEC1 is pressed, turn on the LEDB
       gpioWrite( LEDB, !(gpioRead(TEC1)) );
-      
+
       // If TEC2 is pressed, turn on the LED1
       gpioWrite( LED1, !(gpioRead(TEC2)) );
-      
+
       // If TEC3 is pressed, turn on the LED2
       gpioWrite( LED2, !(gpioRead(TEC3)) );
-      
+
       // If TEC4 is pressed, turn on the LED3
       gpioWrite( LED3, !(gpioRead(TEC4)) );
-   } 
+   }
 
    // IT SHOULD NEVER COME HERE, because this program runs directly on a
    // microcontroller and is not called by any Operating System, as in
