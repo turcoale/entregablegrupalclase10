c21a808161ada789615eaaa2bc81ef7cb971866a
gessi <gessi.paniagua@gmail.com>
2017-04-25 21:00:55 -0300

    Agrego MEF de piso

--- ascensor/app/src/program.c
+++ ascensor/app/src/program.c
@@ -16,12 +16,14 @@ Implementación de MEF en un ascensor
 /*==================[macros and definitions]=================================*/
 typedef enum {EN_ESPERA,MODO_CONFIGURACION,BAJANDO,LEER_PISO,SUBIENDO,
 ABRIENDO_PUERTA,PUERTA_ABIERTA,ALARMA_PUERTA_ABIERTA,CERRANDO_PUERTA,PUERTA_CERRADA} estadoMEF;
+typedef enum {EN_ESPERA1,DIGITO_1,DIGITO_2,GUARDANDO_PISO} estadoMEF1;
 
 #define PISO_INVALIDO (-100)
 #define TIEMPO_MAX_ALARMA (30)
 #define TIEMPO_MAX_ABIERTA (10)
 /*==================[internal data declaration]==============================*/
 estadoMEF estadoActual;
+estadoMEF1 estadoActual1;
 bool configurar= false;
 int nuevo_piso=PISO_INVALIDO;
 int pisoActual=0;
@@ -39,36 +41,6 @@ void actualizarMEF(void);
 
 /*==================[external data definition]===============================*/
 
-/*==================[main function]==========================================*/
-
-// MAIN FUNCTION, ENTRY PINT TO THE PROGRAM AFTER POR OR RESET.
-int main( void ){
-
-   // ---------- SETUP ----------------------------------------
-   // Initialize an configurate the board
-   boardConfig();
-
-   // ---------- REPEAT FOR EVER ------------------------------
-   while( TRUE )
-   {
-      // If TEC1 is pressed, turn on the LEDB
-      gpioWrite( LEDB, !(gpioRead(TEC1)) );
-
-      // If TEC2 is pressed, turn on the LED1
-      gpioWrite( LED1, !(gpioRead(TEC2)) );
-
-      // If TEC3 is pressed, turn on the LED2
-      gpioWrite( LED2, !(gpioRead(TEC3)) );
-
-      // If TEC4 is pressed, turn on the LED3
-      gpioWrite( LED3, !(gpioRead(TEC4)) );
-   }
-
-   // IT SHOULD NEVER COME HERE, because this program runs directly on a
-   // microcontroller and is not called by any Operating System, as in
-   // case of a PC program.
-   return 0;
-}
 
 /*==================[internal functions definition]==========================*/
 void inicializarMEF(void)
@@ -168,8 +140,95 @@ void actualizarMEF(void)
 			inicializarMEF();
 		          break;
 	}
+return;
+}
+/* MAQUINA DE ESTADO DE LOS PISOS*/ 
+void guardarPiso(char piso ){
+
+
+}
+char leertecla(void){
+
+/* usar la sapi para leer el tecledo matricial aca */
+return(0);
 }
 
+
+void inicializarMEF1(void)
+{
+	estadoActual1 = EN_ESPERA1;
+
+
+
+}
+void actualizarMEF1(void)
+{
+	char tecla = leertecla();
+	char digito1=0;
+	char digito2=0;
+	
+	switch(estadoActual1){
+		case EN_ESPERA1:
+			if((tecla <= 9) && (tecla >= 0)){
+				estadoActual1=DIGITO_1;
+				digito1=tecla;
+				
+			}
+			
+			break;
+		case DIGITO_1:
+			if((tecla <= 9) && (tecla >= 0)){
+				estadoActual1=DIGITO_2;
+				digito2=tecla;
+				
+			}
+			if(tecla == 'B'){
+				estadoActual1=EN_ESPERA1;			
+
+			}
+			if(tecla == 'A'){
+				estadoActual1=GUARDANDO_PISO;
+			}			
+			break;
+		case DIGITO_2:
+			if(tecla == 'B'){
+				estadoActual1=EN_ESPERA1;
+				
+			}
+			if(tecla == 'A'){
+				estadoActual1=GUARDANDO_PISO;
+			}			
+			break;
+		case GUARDANDO_PISO:
+			 guardarPiso( (digito1*10)+digito2);
+			 estadoActual1=EN_ESPERA1;
+					
+			break;	
+		default:
+		
+			inicializarMEF1();
+		}
+   return ;
+}
+
+/*==================[main function]==========================================*/
+
+// MAIN FUNCTION, ENTRY PINT TO THE PROGRAM AFTER POR OR RESET.
+int main( void ){
+
+   // ---------- SETUP ----------------------------------------
+   // Initialize an configurate the board
+   boardConfig();
+
+   // ---------- REPEAT FOR EVER ------------------------------
+   while( TRUE ){
+	actualizarMEF();
+	actualizarMEF1();
+	}
+
+ 
+
+}
 
 
 /*==================[external functions definition]==========================*/
